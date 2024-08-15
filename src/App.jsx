1c2876e35e5ce63eae621d2a502be12c1cbba3f6
LuisDavidJM <luisdavidjma@gmail.com>
2024-08-14 19:05:15 -0600

    Formulario funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,57 @@
-import Card from "./Components/Card"
+import React, { useState } from 'react';
+import Card from "./Components/Card";
+import styles from './assets/App.module.css';
+
 function App() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [error, setError] = useState('');
+  const [flag, setFlag] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (title.trim().length < 3 || title.trim().startsWith(' ')) {
+      setError('Por favor chequea que la información sea correcta');
+      setFlag(false);
+      return;
+    }
+    if (description.length < 6) {
+      setError('Por favor chequea que la información sea correcta');
+      setFlag(false);
+      return;
+    }
+
+    setError('');
+    setFlag(true);
+  };
 
   return (
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+    <div className={styles.container}>
+      <h1>Carga de Libros</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder='Titulo del libro'
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder='Descripción del libro'
+          />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+      <p className={styles.errorMessage}>{error ? error : null}</p>
+      {flag ? <Card title={title} description={description} /> : null}
     </div>
-  )
+  );
 }
 
 export default App
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,12 @@
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
-};
+import styles from '../assets/Card.module.css';
+
+function Card({ title, description }) {
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.cardTitle}>El nombre del libro es: {title}</h3>
+      <p className={styles.cardDescription}>La descripción es: {description}</p>
+    </div>
+  );
+}
 
 export default Card;
